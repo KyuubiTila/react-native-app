7748e885bd18491b0b58bfbec82f0a116d62ad7c
KyuubiTila <tylerkatsumoto@gmail.com>
2024-05-13 13:20:16 +0100

    nativewind

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-        <Link style={styles.link} href={'/profile'}>
+    <View className="flex-1 items-center justify-center bg-white">
+      <View >
+        <Text className=" text-3xl" >Hello World</Text>
+        <Text className=" text-xl">This is the first page of your app.</Text>
+        <Link  className="text-blue-500 text-xl" href={'/profile'}>
           Go to Profile
         </Link>
       </View>
@@ -15,28 +15,3 @@ export default function Page() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: 'center',
-    maxWidth: 960,
-    marginHorizontal: 'auto',
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    fontSize: 36,
-    color: '#38434D',
-  },
-  link: {
-    fontSize: 20,
-    color: 'blue',
-  },
-});
