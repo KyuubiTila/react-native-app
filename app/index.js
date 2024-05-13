81dd96b2eb79d47fafe07bbab01eadc843672aaf
KyuubiTila <tylerkatsumoto@gmail.com>
2024-05-13 15:18:59 +0100

    bottom navigation bar layout

--- app/index.js
+++ app/index.js
@@ -7,8 +7,8 @@ export default function Page() {
       <View >
         <Text className=" text-3xl font-pblack" >Hello World</Text>
         <Text className=" text-xl">This is the first page of your app.</Text>
-        <Link  className="text-blue-500 text-xl" href={'/profile'}>
-          Go to Profile
+        <Link  className="text-blue-500 text-xl" href={'/home'}>
+          Go to Home
         </Link>
       </View>
     </View>
